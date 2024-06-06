2baab04b554bb78c2364b83e8ec8b320c3ea27ca
nedretcelik <nedretcelik97@gmail.com>
2024-06-06 13:43:52 -0400

    add link to courses detail

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,36 @@
 "use strict";
 
 window.onload = function () {
-    const courseDetailTB = document.querySelector("#courseDetailTB");
+  const courseDetailTB = document.querySelector("#courseDetailTB");
 
-    function loadCoursesTable() {
-        fetch("http://localhost:8081/api/courses")
-        .then((response) => response.json())
-        .then((courses) => {
-          console.log(courses);
-          for (const course of courses) {
-            let row = courseDetailTB.insertRow();
+  function loadCoursesTable() {
+    fetch("http://localhost:8081/api/courses")
+      .then((response) => response.json())
+      .then((courses) => {
+        console.log(courses);
+        for (const course of courses) {
+          let row = courseDetailTB.insertRow();
 
-            let cell0 = row.insertCell();
-            cell0.innerText = "";
-      
-            let cell1 = row.insertCell();
-            cell1.innerText = course.courseNum;
-      
-            let cell2 = row.insertCell();
-            cell2.innerText = course.courseName;
-      
-            let cell3 = row.insertCell();
-            cell3.innerText = course.instructor;
-      
-            let cell4 = row.insertCell();
-            cell4.innerText = course.startDate;
-      
-            let cell5 = row.insertCell();
-            cell5.innerText = course.numDays;
-          
-          }
-        });
+          let cell0 = row.insertCell();
+          cell0.innerText = "";
 
-    }
+          let cell1 = row.insertCell();
+          cell1.innerText = course.dept;
 
-    loadCoursesTable();
+          let cell2 = row.insertCell();
+          cell2.innerText = course.courseNum;
 
+          let cell3 = row.insertCell();
+          cell3.innerText = course.courseName;
 
-}
+          let anchor = document.createElement("a");
+          anchor.href = `http://localhost:8081/details.html?courseid=${course.id}`;
+          anchor.text = "See details";
+          let cell4 = row.insertCell();
+          cell4.appendChild(anchor);
+        }
+      });
+  }
+
+  loadCoursesTable();
+};
